838f2978d8975c71d6d80bebac32e0ab335e55da
jinhyeon <vmflwu123@gmail.com>
2023-08-25 22:23:25 +0900

    chore(-): api key가 github에 노출된 것을 수정함

--- src/main/java/com/app/trackme/batch/RequestAltitudeItemProcessor.java
+++ src/main/java/com/app/trackme/batch/RequestAltitudeItemProcessor.java
@@ -1,23 +1,20 @@
 package com.app.trackme.batch;
 
+import com.app.trackme.authkey.ApiKey;
 import com.app.trackme.domain.Track;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Component
 public class RequestAltitudeItemProcessor implements ItemProcessor<Track, Track> {
 
-    private final String API_KEY = "AIzaSyDqXtVuWDie8xhCudLEuB83_u6osXD-twQ";
+
     private final String API_URL = "https://maps.googleapis.com/maps/api/elevation/json";
     private final RestTemplate restTemplate = new RestTemplate();
 
@@ -28,7 +25,7 @@ public class RequestAltitudeItemProcessor implements ItemProcessor<Track, Track>
                 .map(coord -> coord.getLatitude() + "," + coord.getLongitude())
                 .collect(Collectors.joining("|"));
         String requestUrl = UriComponentsBuilder.fromUriString(API_URL)
-                .queryParam("key", API_KEY)
+                .queryParam("key", ApiKey.GOOGLE_MAPS_ELEVATION_API_KEY)
                 .queryParam("locations", locationsParam)
                 .build(false)
                 .toUriString();
